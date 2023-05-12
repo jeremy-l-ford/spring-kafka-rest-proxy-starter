500589377536f5dcc39b16d9078ba530ed9c62ba
Jeremy Ford <jeremy.l.ford@gmail.com>
2023-05-11 22:55:27 -0400

    Update dependencies

--- src/main/java/com/github/jeremylford/spring/kafkarestproxy/JerseyConfiguration.java
+++ src/main/java/com/github/jeremylford/spring/kafkarestproxy/JerseyConfiguration.java
@@ -19,20 +19,29 @@
 package com.github.jeremylford.spring.kafkarestproxy;
 
 import io.confluent.kafkarest.KafkaRestConfig;
-import io.confluent.kafkarest.KafkaRestContext;
-import io.confluent.kafkarest.exceptions.ZkExceptionMapper;
-import io.confluent.kafkarest.extension.KafkaRestCleanupFilter;
+import io.confluent.kafkarest.controllers.BrokerManager;
+import io.confluent.kafkarest.controllers.ConsumerManager;
+import io.confluent.kafkarest.controllers.PartitionManager;
+import io.confluent.kafkarest.controllers.TopicConfigManager;
+import io.confluent.kafkarest.controllers.TopicManager;
+import io.confluent.kafkarest.exceptions.RestConstraintViolationExceptionMapper;
 import io.confluent.kafkarest.extension.RestResourceExtension;
+import io.confluent.kafkarest.resources.ResourcesFeature;
+import io.confluent.kafkarest.response.CrnFactory;
+import io.confluent.kafkarest.response.UrlFactory;
 import io.confluent.rest.exceptions.ConstraintViolationExceptionMapper;
 import io.confluent.rest.exceptions.KafkaExceptionMapper;
 import io.confluent.rest.exceptions.WebApplicationExceptionMapper;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.servlet.init.FilterUrlMappingsProviderImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.jersey.JerseyAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 
+import javax.inject.Provider;
+import javax.ws.rs.core.FeatureContext;
 import java.util.List;
 
 @Configuration
@@ -40,21 +49,47 @@ import java.util.List;
 public class JerseyConfiguration extends ResourceConfig {
 
     @Autowired
-    public JerseyConfiguration(KafkaRestConfig kafkaRestConfig, KafkaRestContext kafkaRestContext) {
-        register(new io.confluent.kafkarest.resources.ConsumersResource(kafkaRestContext));
-        register(new io.confluent.kafkarest.resources.v2.ConsumersResource(kafkaRestContext));
-        register(new io.confluent.kafkarest.resources.PartitionsResource(kafkaRestContext));
-        register(new io.confluent.kafkarest.resources.v2.PartitionsResource(kafkaRestContext));
-        register(new io.confluent.kafkarest.resources.BrokersResource(kafkaRestContext));
-        register(new io.confluent.kafkarest.resources.RootResource());
-        register(new io.confluent.kafkarest.resources.TopicsResource(kafkaRestContext));
+    public JerseyConfiguration(
+            KafkaRestConfig kafkaRestConfig,
+            TopicManager topicManager,
+            TopicConfigManager topicConfigManager,
+            CrnFactory crnFactory,
+            UrlFactory urlFactory,
+            BrokerManager brokerManager,
+            PartitionManager partitionManager,
+            ConsumerManager consumerManager
+    ) {
+        Provider<TopicManager> topicManagerProvider = () -> topicManager;
+        Provider<TopicConfigManager> topicConfigManagerProvider = () -> topicConfigManager;
+        Provider<BrokerManager> brokerManagerProvider = () -> brokerManager;
+        Provider<PartitionManager> partitionManagerProvider = () -> partitionManager;
+        Provider<ConsumerManager> consumerManagerProvider = () -> consumerManager;
 
-        register(new ZkExceptionMapper(kafkaRestConfig));
+//        register(new io.confluent.kafkarest.resources.v2.PartitionsResource(partitionManagerProvider));
+//        register(new io.confluent.kafkarest.resources.v2.RootResource());
+
+        register(new io.confluent.kafkarest.resources.v3.ConsumersResource(consumerManagerProvider, crnFactory, urlFactory));
+        register(new io.confluent.kafkarest.resources.v3.PartitionsResource(partitionManagerProvider, crnFactory, urlFactory));
+        register(new io.confluent.kafkarest.resources.v3.BrokersResource(brokerManagerProvider, crnFactory, urlFactory));
+        register(new io.confluent.kafkarest.resources.v2.TopicsResource(topicManagerProvider, topicConfigManagerProvider));
+        register(new io.confluent.kafkarest.resources.v3.TopicsResource(topicManagerProvider, crnFactory, urlFactory));
+
+//        register(new ZkExceptionMapper(kafkaRestConfig));
+        register(new RestConstraintViolationExceptionMapper());
         register(new ConstraintViolationExceptionMapper());
         register(new WebApplicationExceptionMapper(kafkaRestConfig));
         register(new KafkaExceptionMapper(kafkaRestConfig));
-        register(new KafkaRestCleanupFilter());
+//        register(new KafkaRestCleanupFilter());
         register(new FilterUrlMappingsProviderImpl());
+
+
+//        registerFeatures(this, kafkaRestConfig);
+//        registerJsonProvider(this, kafkaRestConfig, true);
+
+        property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+        property(ServerProperties.WADL_FEATURE_DISABLE, true);
+
+//        configureMetrics(kafkaRestConfig);
 
         List<RestResourceExtension> restResourceExtensions = kafkaRestConfig.getConfiguredInstances(
                 KafkaRestConfig.KAFKA_REST_RESOURCE_EXTENSION_CONFIG,
--- src/main/java/com/github/jeremylford/spring/kafkarestproxy/KafkaRestProxyAutoConfiguration.java
+++ src/main/java/com/github/jeremylford/spring/kafkarestproxy/KafkaRestProxyAutoConfiguration.java
@@ -19,73 +19,282 @@
 package com.github.jeremylford.spring.kafkarestproxy;
 
 import com.github.jeremylford.spring.kafkarestproxy.properties.KafkaRestProperties;
-import io.confluent.kafkarest.AdminClientWrapper;
 import io.confluent.kafkarest.KafkaRestConfig;
-import io.confluent.kafkarest.KafkaRestContext;
-import io.confluent.kafkarest.ProducerPool;
-import io.confluent.kafkarest.ScalaConsumersContext;
-import io.confluent.kafkarest.extension.ContextInvocationHandler;
-import io.confluent.kafkarest.extension.KafkaRestContextProvider;
-import io.confluent.kafkarest.v2.KafkaConsumerManager;
-import io.confluent.rest.RestConfigException;
+import io.confluent.kafkarest.controllers.BrokerManager;
+import io.confluent.kafkarest.controllers.ClusterManager;
+import io.confluent.kafkarest.controllers.ConsumerGroupManager;
+import io.confluent.kafkarest.controllers.ConsumerManager;
+import io.confluent.kafkarest.controllers.PartitionManager;
+import io.confluent.kafkarest.controllers.TopicConfigManager;
+import io.confluent.kafkarest.controllers.TopicManager;
+import io.confluent.kafkarest.response.CrnFactory;
+import io.confluent.kafkarest.response.CrnFactoryImpl;
+import io.confluent.kafkarest.response.FakeUrlFactory;
+import io.confluent.kafkarest.response.UrlFactory;
+import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.context.annotation.RequestScope;
 
-import java.lang.reflect.Proxy;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @EnableConfigurationProperties({KafkaRestProperties.class})
 @Configuration
 @Import(JerseyConfiguration.class)
 public class KafkaRestProxyAutoConfiguration {
 
-
+    @SuppressWarnings("unchecked")
     @Bean
-    public KafkaRestConfig kafkaRestConfig(KafkaRestProperties kafkaRestProperties) throws RestConfigException {
-        return new KafkaRestConfig(kafkaRestProperties.asProperties());
+    public ClusterManager clusterManager(Admin adminClient) {
+        return createImpl(
+                "io.confluent.kafkarest.controllers.ClusterManagerImpl",
+                new Class[]{Admin.class},
+                new Object[]{adminClient}
+        );
     }
 
+    @SuppressWarnings("unchecked")
     @Bean
-    public ScalaConsumersContext scalaConsumersContext(KafkaRestConfig kafkaRestConfig) {
-        return new ScalaConsumersContext(kafkaRestConfig);
+    public TopicManager topicManager(Admin adminClient, ClusterManager clusterManager) {
+        return createImpl(
+                "io.confluent.kafkarest.controllers.TopicManagerImpl",
+                new Class[]{Admin.class, ClusterManager.class},
+                new Object[]{adminClient, clusterManager}
+        );
     }
 
+    @SuppressWarnings("unchecked")
     @Bean
-    public AdminClientWrapper adminClientWrapper(KafkaRestConfig kafkaRestConfig, AdminClient adminClient) {
-        return new AdminClientWrapper(kafkaRestConfig, adminClient);
-    }
-
-    @Bean
-    public KafkaConsumerManager kafkaConsumerManager(KafkaRestConfig kafkaRestConfig) {
-        return new KafkaConsumerManager(kafkaRestConfig);
-    }
-
-    @Bean
-    public ProducerPool producerPool(KafkaRestConfig kafkaRestConfig) {
-        return new ProducerPool(kafkaRestConfig);
-    }
-
-    @Bean
-    public KafkaRestContext kafkaRestContext(KafkaRestConfig kafkaRestConfig,
-                                             ProducerPool producerPool,
-                                             KafkaConsumerManager kafkaConsumerManager,
-                                             AdminClientWrapper adminClientWrapper,
-                                             ScalaConsumersContext scalaConsumersContext) {
-        KafkaRestContextProvider.initialize(kafkaRestConfig, producerPool, kafkaConsumerManager, adminClientWrapper, scalaConsumersContext);
-
-        ContextInvocationHandler contextInvocationHandler = new ContextInvocationHandler();
-        return (KafkaRestContext) Proxy.newProxyInstance(
-                KafkaRestContext.class.getClassLoader(),
-                new Class[]{KafkaRestContext.class},
-                contextInvocationHandler
+    public TopicConfigManager topicConfigManager(Admin adminClient, ClusterManager clusterManager) {
+        return createImpl(
+                "io.confluent.kafkarest.controllers.TopicConfigManagerImpl",
+                new Class[]{Admin.class, ClusterManager.class},
+                new Object[]{adminClient, clusterManager}
         );
     }
 
     @Bean
-    public AdminClient adminClient(KafkaRestConfig kafkaRestConfig) {
-        return AdminClient.create(AdminClientWrapper.adminProperties(kafkaRestConfig));
+    public BrokerManager brokerManager(ClusterManager clusterManager) {
+        return createImpl("io.confluent.kafkarest.controllers.BrokerManagerImpl", new Class[]{ClusterManager.class}, new Object[]{clusterManager});
+    }
+
+    @Bean
+    public PartitionManager partitionManager(Admin admin, TopicManager topicManager) {
+        return createImpl("io.confluent.kafkarest.controllers.PartitionManagerImpl", new Class[]{Admin.class, TopicManager.class}, new Object[]{admin, topicManager});
+    }
+
+    @Bean
+    public ConsumerGroupManager consumerGroupManager(Admin admin, ClusterManager clusterManager) {
+        return createImpl("io.confluent.kafkarest.controllers.ConsumerGroupManagerImpl", new Class[]{Admin.class, ClusterManager.class}, new Object[]{admin, clusterManager});
+    }
+
+    @Bean
+    public ConsumerManager consumerManager(ConsumerGroupManager consumerGroupManager) {
+        return createImpl("io.confluent.kafkarest.controllers.ConsumerManagerImpl", new Class[]{ConsumerGroupManager.class}, new Object[]{consumerGroupManager});
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T createImpl(String impl, Class<?>[] parameterTypes, Object[] parameters) {
+
+
+        try {
+            Class<T> clazz = (Class<T>) Class.forName(impl);
+            Constructor<T> declaredConstructor = clazz.getDeclaredConstructor(parameterTypes);
+            declaredConstructor.setAccessible(true);
+            MethodHandle constructor = MethodHandles.lookup().unreflectConstructor(declaredConstructor);
+            declaredConstructor.setAccessible(false);
+
+//            MethodHandle constructor = MethodHandles.lookup().findConstructor(clazz, MethodType.methodType(void.class, parameterTypes));
+            return (T) constructor.invokeWithArguments(parameters);
+
+//            Constructor<T> declaredConstructor = clazz.getDeclaredConstructor(parameterTypes);
+//            declaredConstructor.setAccessible(true);
+//            return declaredConstructor.newInstance(parameters);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Bean
+    public CrnFactory crnFactory(KafkaRestConfig config) {
+        String crnConfig = config.getString(KafkaRestConfig.CRN_AUTHORITY_CONFIG);
+        return new CrnFactoryImpl(crnConfig);
+    }
+
+    @Bean
+    @RequestScope
+    public UrlFactory urlFactory(KafkaRestConfig config, HttpServletRequest httpServletRequest) {
+        //@HostNameConfig String hostNameConfig,
+        //      @PortConfig Integer portConfig,
+        //      @AdvertisedListenersConfig List<URI> advertisedListenersConfig,
+        //      @ListenersConfig List<URI> listenersConfig,
+        //      @Context UriInfo requestUriInfo
+        String hostName = config.getString(KafkaRestConfig.HOST_NAME_CONFIG);
+        int port = config.getInt(KafkaRestConfig.PORT_CONFIG);
+        List<URI> advertisedListeners = config.getList(KafkaRestConfig.ADVERTISED_LISTENERS_CONFIG).stream().map(URI::create).collect(Collectors.toList());
+        List<URI> listeners = config.getList(KafkaRestConfig.LISTENERS_CONFIG).stream().map(URI::create).collect(Collectors.toList());
+        UriInfo uriInfo = new UriInfo() {
+            @Override
+            public String getPath() {
+                return httpServletRequest.getRequestURI();
+            }
+
+            @Override
+            public String getPath(boolean decode) {
+                return getRequestUriBuilder().build().getPath();
+            }
+
+            @Override
+            public List<PathSegment> getPathSegments() {
+                return null;
+            }
+
+            @Override
+            public List<PathSegment> getPathSegments(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public URI getRequestUri() {
+                return URI.create(httpServletRequest.getRequestURI());
+            }
+
+            @Override
+            public UriBuilder getRequestUriBuilder() {
+                return UriBuilder.fromUri(httpServletRequest.getRequestURI());
+            }
+
+            @Override
+            public URI getAbsolutePath() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getAbsolutePathBuilder() {
+                return null;
+            }
+
+            @Override
+            public URI getBaseUri() {
+                return null;
+            }
+
+            @Override
+            public UriBuilder getBaseUriBuilder() {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getPathParameters() {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getPathParameters(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getQueryParameters() {
+                return null;
+            }
+
+            @Override
+            public MultivaluedMap<String, String> getQueryParameters(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public List<String> getMatchedURIs() {
+                return null;
+            }
+
+            @Override
+            public List<String> getMatchedURIs(boolean decode) {
+                return null;
+            }
+
+            @Override
+            public List<Object> getMatchedResources() {
+                return null;
+            }
+
+            @Override
+            public URI resolve(URI uri) {
+                return null;
+            }
+
+            @Override
+            public URI relativize(URI uri) {
+                return null;
+            }
+        };
+
+        return new FakeUrlFactory();
+//        return new UrlFactoryImpl(hostName, port, advertisedListeners, listeners, uriInfo);
+    }
+
+
+    @Bean
+    public KafkaRestConfig kafkaRestConfig(KafkaRestProperties kafkaRestProperties) {
+        return new KafkaRestConfig(kafkaRestProperties.asProperties());
+    }
+
+    //
+//    @Bean
+//    public ScalaConsumersContext scalaConsumersContext(KafkaRestConfig kafkaRestConfig) {
+//        return new ScalaConsumersContext(kafkaRestConfig);
+//    }
+//
+//    @Bean
+//    public AdminClientWrapper adminClientWrapper(KafkaRestConfig kafkaRestConfig, AdminClient adminClient) {
+//        return new AdminClientWrapper(kafkaRestConfig, adminClient);
+//    }
+//
+//    @Bean
+//    public KafkaConsumerManager kafkaConsumerManager(KafkaRestConfig kafkaRestConfig) {
+//        return new KafkaConsumerManager(kafkaRestConfig);
+//    }
+//
+//    @Bean
+//    public ProducerPool producerPool(KafkaRestConfig kafkaRestConfig) {
+//        return new ProducerPool(kafkaRestConfig);
+//    }
+//
+//    @Bean
+//    public KafkaRestContext kafkaRestContext(KafkaRestConfig kafkaRestConfig,
+//                                             ProducerPool producerPool,
+//                                             KafkaConsumerManager kafkaConsumerManager,
+//                                             AdminClientWrapper adminClientWrapper,
+//                                             ScalaConsumersContext scalaConsumersContext) {
+//        KafkaRestContextProvider.initialize(kafkaRestConfig, producerPool, kafkaConsumerManager, adminClientWrapper, scalaConsumersContext);
+//
+//        ContextInvocationHandler contextInvocationHandler = new ContextInvocationHandler();
+//        return (KafkaRestContext) Proxy.newProxyInstance(
+//                KafkaRestContext.class.getClassLoader(),
+//                new Class[]{KafkaRestContext.class},
+//                contextInvocationHandler
+//        );
+//    }
+//
+    @Bean
+    public Admin adminClient(KafkaRestConfig kafkaRestConfig) {
+        return AdminClient.create(kafkaRestConfig.getAdminProperties());
     }
 
 }
